77c54fe02449146f420887253acf0b1f9e5b5795
31373773 <55849726+31373773@users.noreply.github.com>
2020-03-03 14:57:15 -0500

    Task: Calculator.py

---
+++ Tests/test_Calculator.py
@@ -0,0 +1,12 @@
+import unittest
+
+from Calculator.Calculator import Calculator
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.calculator = Calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, Calculator)
