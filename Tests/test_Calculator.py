a12b04c561053e8b4f5aeea131688443fa6a2966
31373773 <55849726+31373773@users.noreply.github.com>
2020-03-10 14:51:45 -0400

    Task 5: Create subtraction function and test

--- Tests/test_Calculator.py
+++ Tests/test_Calculator.py
@@ -10,3 +10,11 @@ class MyTestCase(unittest.TestCase):
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.calculator, Calculator)
+
+    def test_calculator_return_sum(self):
+        result = self.calculator.add(1, 2)
+        self.assertEqual(3, result)
+
+    def test_calculator_return_difference(self):
+        result = self.calculator.subtract(2, 1)
+        self.assertEqual(1, result)
--- Tests/test_MathOperations.py
+++ Tests/test_MathOperations.py
@@ -1,8 +1,11 @@
 import unittest
-
-from MathOperations.addition import Addition
+from MathOperations.Subtraction import Subtraction
+from MathOperations.Addition import Addition
 
 class MyTestCase(unittest.TestCase):
 
     def test_MathOperations_Addition(self):
         self.assertEqual(3, Addition.sum(1, 2))
+
+    def test_MathOperations_subtraction(self):
+        self.assertEqual(1, Subtraction.difference(2, 1))
